f7b086ca19ab56b794e082e6cb6a55526c401922
Sumit <sumitepg@gmail.com>
2022-05-21 09:12:40 +0530

    Feedback implemented

--- IllustratorPreflight.jsx
+++ IllustratorPreflight.jsx
@@ -9,6 +9,10 @@ function preflight() {
             ExternalObject.AdobeXMPScript = new ExternalObject('lib:AdobeXMPScript');
         }
         var myDoc = app.documents[0];
+        if (!myDoc.saved) {
+            alert("Document is not saved, please save the document and re-run.");
+            return;
+        }
         myDoc.rulerOrigin = [0, myDoc.height];
         app.preferences.setIntegerPreference("rulerType", 0);
         var rgb = [], fogra = [], lowres = [];
@@ -46,25 +50,47 @@ function preflight() {
             }
         }
         var logText = "";
+        var d = new Date();
+        yy = d.getFullYear();
+        mm = ('0' + (d.getMonth() + 1)).substr(-2);
+        dd = ('0' + d.getDate()).substr(-2);
+        hh = ('0' + d.getHours()).substr(-2);
+        mn = ('0' + d.getMinutes()).substr(-2);
+        ss = ('0' + d.getSeconds()).substr(-2);
+        logText += myDoc.name + " " + yy + "-" + mm + "-" + dd + "_" + hh + ":" + mn + ":" + ss + "\r=========================================\r\r";
         if (rgb.length > 0 || fogra.length > 0 || lowres.length > 0) {
             if (fogra.length > 0 || lowres.length > 0) {
                 if (fogra.length > 0) {
-                    logText += "Other Profile Found in Images\r=========================================\r"
+                    logText += "Other Profile Found in Images\r=========================================\r";
                     logText += fogra.join("\r");
                     logText += "\r\r";
                 }
                 if (lowres.length > 0) {
-                    logText += "Low-Res Information\r=========================================\r"
+                    logText += "Low-Res Information\r=========================================\r";
                     logText += lowres.join("\r");
                 }
                 displayLog(myDoc, logText);
             }
         }
         else {
-            removeUnusedSwatches();
-            setCutterColorToOverprint();
-            myDoc.save();
-            exportToPDF(myDoc);
+            try {
+                logText += "RGB Images checked -- SUCCESS\r";
+                logText += "Fogra Profile checked -- SUCCESS\r";
+                logText += "Low-Res Images checked -- SUCCESS\r";
+                removeUnusedSwatches();
+                logText += "Unused Swatches checked and removed -- SUCCESS\r";
+                setCutterColorToOverprint();
+                logText += "Cutter Swatches fill and stroke set overprint -- SUCCESS\r";
+                myDoc.save();
+                exportToPDF(myDoc);
+                logText += "PDF Exported with the same name replaced .ai to .pdf -- SUCCESS\r";
+            }
+            catch (e) {
+
+            }
+            finally {
+                displayLog(app.activeDocument, logText);
+            }
         }
     }
     else {
@@ -74,8 +100,8 @@ function preflight() {
 
 function crossOverRGBImages(obj, left, top, w, h) {
     try {
-        var colorMode = Number(obj.getProperty(XMPConst.NS_PHOTOSHOP, "ColorMode"));
-        if (colorMode == 3) {
+        var colorMode = obj.getProperty(XMPConst.NS_PHOTOSHOP, "ColorMode");
+        if (colorMode == undefined || Number(colorMode) == 3) {
             crossMark(left, top, w, h);
             return true;
         }
@@ -89,7 +115,7 @@ function crossOverRGBImages(obj, left, top, w, h) {
 function isNonFogra39Images(obj) {
     try {
         var profile = obj.getProperty(XMPConst.NS_PHOTOSHOP, "ICCProfile");
-        if (!(/FOGRA/ig.test(profile.toString()))) {
+        if (profile == undefined || !(/FOGRA/ig.test(profile.toString()))) {
             return true;
         }
     }
@@ -101,9 +127,9 @@ function isNonFogra39Images(obj) {
 
 function isLowResImages(obj, width) {
     try {
-        var actualPPI = Number(obj.getProperty(XMPConst.NS_EXIF, "PixelXDimension")) / width * 72;
-        if (actualPPI < 250) {
-            return [true, Math.round(actualPPI)];
+        var actualPPI = obj.getProperty(XMPConst.NS_EXIF, "PixelXDimension");
+        if (!actualPPI || (actualPPI = Number(actualPPI) / width * 72) < 300) {
+            return [true, actualPPI ? actualPPI : "Uknown"];
         }
     }
     catch (e) {
@@ -117,14 +143,17 @@ function crossMark(left, top, width, height) {
         var cmykRed = new CMYKColor();
         cmykRed.magenta = 100;
         cmykRed.yellow = 100;
+        cmykRed.cyan = 0;
+        cmykRed.black = 0;
         var doc = app.activeDocument;
+        doc.defaultStrokeColor = cmykRed;
         var group = doc.groupItems.add();
         var right = left + width;
         var bottom = -(top + height);
         var path1 = doc.pathItems.add();
         path1.strokeWidth = 6;
         path1.stroked = true;
-        path1.strokeColor = cmykRed;
+        //path1.strokeColor = cmykRed;
         var p1 = path1.pathPoints.add();
         p1.anchor = p1.rightDirection = p1.leftDirection = [left, -top];
         var p2 = path1.pathPoints.add();
@@ -134,7 +163,7 @@ function crossMark(left, top, width, height) {
         path1 = doc.pathItems.add();
         path1.strokeWidth = 6;
         path1.stroked = true;
-        path1.strokeColor = cmykRed;
+        //path1.strokeColor = cmykRed;
         p1 = path1.pathPoints.add();
         p1.anchor = p1.rightDirection = p1.leftDirection = [left, bottom];
         p2 = path1.pathPoints.add();
@@ -162,15 +191,22 @@ function exportToPDF(myDoc) {
     try {
         //show a dialog for 
         var preset = getPresetName();
+        if (preset == "") return;
         var options = new PDFSaveOptions();
         options.pDFPreset = preset;
-        var pdfFile = new File(myDoc.fullName.fsName.replace(/\.ai$/i, ".pdf"));
+        var aiFile = myDoc.fullName;
+        var pdfFile = new File(aiFile.fsName.replace(/\.ai$/i, ".pdf"));
         myDoc.saveAs(pdfFile, options);
+        app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+        app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
+        app.open(aiFile);
+        pdfFile.execute();
+        return "success";
     }
     catch (e) {
         alert(e);
     }
-
+    return "";
 }
 
 function getPresetName() {
